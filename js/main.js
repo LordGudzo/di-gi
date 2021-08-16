1d6da8d4841eb0924331aa43a517cf4ad2f6581e
LordGudzo <LordGudzo@ukr.net>
2021-08-17 01:51:22 +0300

    added carusel

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
-$(function(){
-
+$(document).ready(function(){
+  $(".owl-carousel").owlCarousel({
+    items: 1,
+    loop: true,
+    mouseDrag: false,
+    nav: true,
+    navContainer: '.intro__arrow', 
+    dots: true
+  });
 });
